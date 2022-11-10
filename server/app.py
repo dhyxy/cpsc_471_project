965b3ae1231a5e43a29bbf058311991a317e605d
dhyey <dhyeylalseta@outlook.com>
2022-11-10 12:48:37 -0700

    [demo] adding a new page

--- server/app.py
+++ server/app.py
@@ -6,3 +6,11 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def home():
     return render_template('home.html.jinja')
+
+@core.route('/photographers')
+def photographers():
+
+    # this is static data, but this will be fetched from the database in the actual app
+    # probably will be the easiest way to prototype new pages
+    photographers = [{'name': 'asdlkfj', 'email': 'email@email.com'}, {'name': 'namey name', 'email': 'emaily@email.com'}]
+    return render_template('photographers.html.jinja', photographers=photographers)
